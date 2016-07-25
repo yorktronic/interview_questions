07dc71eaf88b03e1ba61b1db3e52688553e29592
yorktronic <ty.walker@gmail.com>
2016-07-25 14:12:39 -0700

    finished highest_product()

--- README.md
+++ README.md
@@ -80,3 +80,27 @@ get_max_profit(stock_prices_yesterday)
 # returns 6 (buying for $5 and selling for $11)
 ```
 My solution can be found with other my Interview Cake solutions in <a href="interview-cake-questions.py">interview-cake-questions.py</a>
+
+## Fun with Integers ##
+You have a list of integers, and for each index you want to find the product of every integer except the integer at that index.  
+
+Write a function get_products_of_all_ints_except_at_index() that takes a list of integers and returns a list of the products.
+
+For example, given:
+  
+`[1, 7, 3, 4]`  
+
+Your function would return:  
+
+`[84, 12, 28, 21]`
+
+by calculating:
+
+`[7*3*4, 1*3*4, 1*7*4, 1*7*3]`  
+
+**Do not use division in your solution.**
+
+## Highest Product ##
+Given a list_of_ints, find the highest_product you can get from three of the integers.  
+
+The input list_of_ints will always have at least three integers.
--- interview-cake-questions.py
+++ interview-cake-questions.py
@@ -1,4 +1,12 @@
+#######################################################################################################################
+#
 # Stock Trading Function -- see the README of the interview_questions repo for the problem description
+# My initial solution: 14.1 microseconds
+# My recursive solution: 14.3 microseconds
+# Interview Cake's solution: 6.33 microseconds
+#
+#######################################################################################################################
+
 # This was my first solution, which is not optimal as it's an O(2n) solution instead of their optimal O(n) solution
 stock_prices_yesterday = [10, 7, 5, 8, 11, 9, 1, 0, 99, 1, 200, 3]
 stock_prices_decreasing = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
@@ -59,3 +67,117 @@ def get_max_profit_cake(stock_prices_yesterday):
         min_price = min(min_price, current_price)
 
     return max_profit
+
+#######################################################################################################################
+#
+# Fun with Integers
+# My solution: 10.1 microseconds
+# Interview Cake's solution: 4.06 microseconds
+#
+#######################################################################################################################
+
+# My solution
+def get_products_of_all_ints_except_at_index(nums):
+    from operator import mul
+    products = []
+    for i, num in enumerate(nums):
+        reduced_nums = nums[:i] + nums[i+1 :]
+        products.append(reduce(mul, reduced_nums))
+    return products
+
+# Interview Cake's Solution, of complexity O(n) time and O(n) space
+def get_products_of_all_ints_except_at_index_cake(int_list):
+    # we make a list with the length of the input list to
+    # hold our products
+    products_of_all_ints_except_at_index = [None] * len(int_list)
+
+    # for each integer, we find the product of all the integers
+    # before it, storing the total product so far each time
+    product_so_far = 1
+    i = 0
+    while i < len(int_list):
+        products_of_all_ints_except_at_index[i] = product_so_far
+        product_so_far *= int_list[i]
+        i += 1
+
+    # for each integer, we find the product of all the integers
+    # after it. since each index in products already has the
+    # product of all the integers before it, now we're storing
+    # the total product of all other integers
+    product_so_far = 1
+    i = len(int_list) - 1
+    while i >= 0:
+        products_of_all_ints_except_at_index[i] *= product_so_far
+        product_so_far *= int_list[i]
+        i -= 1
+
+    return products_of_all_ints_except_at_index
+
+#######################################################################################################################
+#
+# Highest Product (I'm finally faster than Interview Cake!)
+# My solution: 2.96 microseconds if there are no negative numbers, 3.24 microseconds otherwise
+# Interview Cake's solution: 5.71 microseconds if there are no negative numbers, 6.05 microseconds otherwise
+#
+#######################################################################################################################
+
+# My solution
+def highest_product(nums):
+    nums_asc = sorted(nums)
+    nums_desc = sorted(nums, reverse=True)
+
+    if nums_asc[0] * nums_asc[1] > nums_desc[0] * nums_desc[1]:
+        return nums_asc[0] * nums_asc[1] * nums_desc[0]
+    else:
+        return nums_desc[0] * nums_desc[1] * nums_desc[2]
+
+# Interview Cake's solution
+def highest_product_of_3(list_of_ints):
+    if len(list_of_ints) < 3:
+        raise Exception('Less than 3 items!')
+
+    # We're going to start at the 3rd item (at index 2)
+    # so pre-populate highests and lowests based on the first 2 items.
+    # we could also start these as None and check below if they're set
+    # but this is arguably cleaner
+    highest = max(list_of_ints[0], list_of_ints[1])
+    lowest = min(list_of_ints[0], list_of_ints[1])
+
+    highest_product_of_2 = list_of_ints[0] * list_of_ints[1]
+    lowest_product_of_2 = list_of_ints[0] * list_of_ints[1]
+
+    # except this one--we pre-populate it for the first /3/ items.
+    # this means in our first pass it'll check against itself, which is fine.
+    highest_product_of_three = list_of_ints[0] * list_of_ints[1] * list_of_ints[2]
+
+    # walk through items, starting at index 2
+    for current in list_of_ints[2:]:
+        # do we have a new highest product of 3?
+        # it's either the current highest,
+        # or the current times the highest product of two
+        # or the current times the lowest product of two
+        highest_product_of_three = max(
+            highest_product_of_three,
+            current * highest_product_of_2,
+            current * lowest_product_of_2)
+
+        # do we have a new highest product of two?
+        highest_product_of_2 = max(
+            highest_product_of_2,
+            current * highest,
+            current * lowest)
+
+        # do we have a new lowest product of two?
+        lowest_product_of_2 = min(
+            lowest_product_of_2,
+            current * highest,
+            current * lowest)
+
+        # do we have a new highest?
+        highest = max(highest, current)
+
+        # do we have a new lowest?
+        lowest = min(lowest, current)
+
+    return highest_product_of_three
+
