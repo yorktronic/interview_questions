380573556708807440d0dae8f953db0c33b47fcb
yorktronic <ty.walker@gmail.com>
2016-12-08 18:10:14 -0800

    found optimal on stackoverflow

--- fib.py
+++ fib.py
@@ -1,6 +1,7 @@
 # Write a function that takes a number n and returns an array
 # containing a Fibonacci sequence of length
 
+# My fastest
 def fibonacci(n):
     i = 2
     sequence = [0, 1]
@@ -10,3 +11,30 @@ def fibonacci(n):
 
     return sequence
 
+# My slowest
+def fibonacci_faster(n):
+    sequence = [0] * n
+
+    for idx, num in enumerate(sequence):
+        if idx == 0:
+            sequence[idx] = 0
+        elif idx == 1:
+            sequence[idx] = 1
+        else:
+            sequence[idx] += sequence[idx - 1] + sequence[idx - 2]
+
+    return sequence
+
+# Optimal
+def fibo(n):
+    a, b = 0, 1
+    for i in xrange(n):
+        yield a
+        a, b = b, a + b
+
+def generate(n):
+    seq = []
+    for i in fibo(n):
+        seq.append(i)
+
+    return seq
