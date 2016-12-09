ca55a91dfcaf4735ff3a06aa1bdad5e75b1c57b4
yorktronic <ty.walker@gmail.com>
2016-12-08 20:49:01 -0800

    derp

--- datatron.py
+++ datatron.py
@@ -3,34 +3,33 @@
 ## This is the text editor interface.
 ## Anything you type or change here will be seen by the other person in real time.
 
-F
-/ \
-    B
-H
-/  \ /
-A
-D
-G
-/ \
-    C
-E
+#F
+#/ \
+#   B
+#H
+#/  \ /
+#A
+#D
+#G
+#/ \
+#    C
+# E
 
 # Turn tree in to circular doubly-linked-list
 
-A <->B <->C <->D <->F <->G <->H <->A
+# A <-> B <-> C <-> D <-> F <-> G <-> H <-> A
 
-F -> B
-B -> A
+# F -> B
+# B -> A
 
-A <->F
-
+# A <-> F
 
 def build_cdll(root):
     # in: root of a tree
     # out: head
 
     if root.left == None and root.right == None:
-    # we've found the head of the CDLL
+        # we've found the head of the CDLL
     elif root.left == None and root.right is not None:
         # we've found the head and the next element may be root.right
         build_cdll(root.right)
@@ -38,7 +37,3 @@ def build_cdll(root):
         build_cdll(root.left)
 
     build_cdll(root.left)
-
-
-tree = {'F': ['B', 'H'],
-        'B': ['F', 'A'],
