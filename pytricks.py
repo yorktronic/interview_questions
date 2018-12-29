1a25bad8f8590c368a379c745959a75595de7660
yorktronic <ty.walker@gmail.com>
2018-12-29 15:21:27 -0800

    more stuff

--- pytricks.py
+++ pytricks.py
@@ -1,13 +1,34 @@
 # How to sort a Python dict by value
 # (== get a representation sorted by value)
 
->>> xs = {'a': 4, 'b': 3, 'c': 2, 'd': 1}
+xs = {'a': 4, 'b': 3, 'c': 2, 'd': 1}
 
->>> sorted(xs.items(), key=lambda x: x[1])
-[('d', 1), ('c', 2), ('b', 3), ('a', 4)]
+sorted(xs.items(), key=lambda x: x[1])
+#[('d', 1), ('c', 2), ('b', 3), ('a', 4)]
 
 # Or:
 
->>> import operator
->>> sorted(xs.items(), key=operator.itemgetter(1))
-[('d', 1), ('c', 2), ('b', 3), ('a', 4)]
+import operator
+sorted(xs.items(), key=operator.itemgetter(1))
+#[('d', 1), ('c', 2), ('b', 3), ('a', 4)]
+
+# Why Python is Great: Namedtuples
+# Using namedtuple is way shorter than
+# defining a class manually:
+>>> from collections import namedtuple
+>>> Car = namedtuple('Car', 'color mileage')
+
+# Our new "Car" class works as expected:
+>>> my_car = Car('red', 3812.4)
+>>> my_car.color
+'red'
+>>> my_car.mileage
+3812.4
+
+# We get a nice string repr for free:
+>>> my_car
+Car(color='red' , mileage=3812.4)
+
+# Like tuples, namedtuples are immutable:
+>>> my_car.color = 'blue'
+AttributeError: "can't set attribute"
